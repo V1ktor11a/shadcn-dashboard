cddeef9a88df434fbde29c424c6704ea80f4bef0
Vika Sabadosh <vika.kahanec@gmail.com>
2024-07-30 18:46:37 +0300

    finished app

---
+++ app/(main)/posts/page.tsx
@@ -0,0 +1,16 @@
+import BackButton from '@/components/BackButton';
+import PostsPagination from '@/components/posts/PostsPagination';
+import PostsTable from '@/components/posts/PostsTable';
+
+const PostsPage = () => {
+  return (
+    <>
+      <BackButton text='Go Back' link='/' />
+      <PostsTable />
+      <PostsPagination />
+    </>
+  );
+};
+
+PostsTable;
+export default PostsPage;
